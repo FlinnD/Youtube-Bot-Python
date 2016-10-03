8b199cbd1a5ea2b26e5dc8e5ac3bfb4138ce5268
FlinnD <fd651@york.ac.uk>
2016-10-03 17:02:40 +0100

    Fix errors in code; make functional

--- Scripts/youtubeChannelAnalytics.py
+++ Scripts/youtubeChannelAnalytics.py
@@ -1,10 +1,10 @@
 from apiclient.discovery import build
-from apiclient.errors import Httperror
+#from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 
 
 
-API_KEY = "xxxxxx"
+API_KEY = "xxxxxxxxxx"
 
 API_NAME = "youtube"
 
@@ -12,29 +12,31 @@ API_VERSION = "v3"
 
 
 def main(options):
+	title = ''
+	subs = ''
+	videoNo = ''
+	views = ''
+	conn = build(API_NAME, API_VERSION, developerKey = API_KEY)
 
-conn = build(API_NAME, API_VERSION, developerKey = API_KEY)
-
-channel = conn.channels().list(
-	id = options.channelid,
-	part = "snippet,statistics"
-).execute()
-
-title = channel["items"]["snippet"]["title"] 	
-subs = channel["items"]["statistics"]["subscriberCount"] 		
-videoNo = channel["items"]["statistics"]["videoCount"] 		
-views = channel["items"]["statistics"]["viewCount"] 
-
-print "/n title: {} /n Subscribers: {} /n Number of videos: {} /n Number of views: {}"
+	channel = conn.channels().list(
+		id = options.channelid,
+		part = "snippet,statistics"
+	).execute()
+	for i in channel.get("items",[]):
+		title = i["snippet"]["title"] 	
+		subs = i["statistics"]["subscriberCount"] 		
+		videoNo = i["statistics"]["videoCount"] 		
+		views = i["statistics"]["viewCount"] 
+	print("\n title: {} \n Subscribers: {} \n Number of videos: {} \n Number of views: {}").format(title,subs,videoNo,views)
 
 
-if __NAME__ == __MAIN__:
+if __name__ == '__main__':
 	
 	argparser.add_argument("--channelid", help = "id of the specified channel")
-	argparser.add_argument("--results", help = "number of channels before stopping", default = "25")
+	#argparser.add_argument("--results", help = "number of channels before stopping", default = "25")
 	options = argparser.parse_args()
-	
+		
 	try:
 		main(options)
-	except HttpError e:
+	except HttpError as e:
 		print "HTTP Error {} occured:/n{}".format(e.resp.status,e.content)
